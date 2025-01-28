1a37898284d2164cbfa5dc983349c6c52b9bf30f
Raphael Antunes <faelpinheiroantunes@gmail.com>
2025-01-27 21:52:35 -0300

    Criando as migrates

---
+++ database/migrations/2025_01_28_003941_create_consultas_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsultasTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('consultas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medico_id')->constrained('medicos')->onDelete('cascade');
+            $table->foreignId('paciente_id')->constrained('pacientes')->onDelete('cascade');
+            $table->timestamp('data');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('consultas');
+    }
+}
